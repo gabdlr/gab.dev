fef31448fe979121db3ce443388c9cd5839e474f
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-04-07 00:42:26 -0300

    build: adds material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatGridListModule],
 })
 export class AppComponent {
-  title = 'gabdev';
+  title = 'Gabriel De Los Rios, Web Developer';
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { routes } from './app/app-routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)],
 });
