86ee49e8b41a3b91d2c7c6a0bb7cff081b4446bd
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-04-15 20:13:11 -0300

    build: removes router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterOutlet, MatGridListModule],
+  imports: [CommonModule, MatGridListModule],
 })
 export class AppComponent {
   title = 'Gabriel De Los Rios, Web Developer';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 /// <reference types="@angular/localize" />
 
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { routes } from './app/app-routing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)],
+  providers: [importProvidersFrom(BrowserAnimationsModule)],
 });
