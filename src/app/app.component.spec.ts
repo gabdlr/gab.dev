a926f7cc5cb79523333b3db54a46d2fac17d2840
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-04-22 22:07:43 -0300

    test: language-selector and app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gabdev'`, () => {
+  it(`should have as title 'Gabriel De Los Rios, Web Developer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gabdev');
+    expect(app.title).toEqual('Gabriel De Los Rios, Web Developer');
   });
 
-  it('should render title', () => {
+  it('should render presentation title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gabdev app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      //prettier-ignore
+      "Hi, I\'m Gabriel De Los Rios and I build enterprise grade frontend applications"
+    );
   });
 });
--- src/app/components/language-selector/language-selector.component.spec.ts
+++ src/app/components/language-selector/language-selector.component.spec.ts
@@ -1,23 +1,93 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LanguageSelectorComponent } from './language-selector.component';
-
+import { DebugElement } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { By } from '@angular/platform-browser';
 describe('LanguageSelectorComponent', () => {
   let component: LanguageSelectorComponent;
   let fixture: ComponentFixture<LanguageSelectorComponent>;
+  let el: DebugElement;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ LanguageSelectorComponent ]
-    })
-    .compileComponents();
+      imports: [LanguageSelectorComponent, MatMenuModule, NoopAnimationsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LanguageSelectorComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show us flag when current language is en', () => {
+    component.currentLang = 'en';
+    fixture.detectChanges();
+    const debugElement = el.nativeElement as HTMLElement;
+    const flagImg = debugElement.querySelector('img') as HTMLImageElement;
+    expect(flagImg.alt).toBe('us-flag');
+  });
+
+  it('should display spanish option when english is current', async () => {
+    component.currentLang = 'en';
+    let matMenu = await loader.getHarness(MatMenuHarness);
+    await matMenu.open();
+    const option = el.query(By.css('.mat-mdc-menu-item'));
+    const optionImage: HTMLImageElement | null = (
+      option.nativeElement as HTMLElement
+    ).querySelector('img');
+    expect(optionImage?.alt).toBe('spain-flag');
+  });
+
+  //prettier-ignore
+  it("should have \'es\' as spanish menu option href", async () => {
+    component.currentLang = 'en';
+    let matMenu = await loader.getHarness(MatMenuHarness);
+    await matMenu.open();
+    const option = el.query(By.css('.mat-mdc-menu-item'));
+    const optionHref = (option.nativeElement as HTMLLinkElement).href.slice(
+      (option.nativeElement as HTMLLinkElement).href.lastIndexOf('/') + 1
+    );
+    expect(optionHref).toBe('es');
+  });
+
+  it('should show spanish flag when current language is es', () => {
+    component.currentLang = 'es';
+    fixture.detectChanges();
+    const debugElement = el.nativeElement as HTMLElement;
+    const flagImg = debugElement.querySelector('img') as HTMLImageElement;
+    expect(flagImg.alt).toBe('spain-flag');
+  });
+
+  it('should display english option when spanish is current', async () => {
+    component.currentLang = 'es';
+    let matMenu = await loader.getHarness(MatMenuHarness);
+    await matMenu.open();
+    const option = el.query(By.css('.mat-mdc-menu-item'));
+    const optionImage: HTMLImageElement | null = (
+      option.nativeElement as HTMLElement
+    ).querySelector('img');
+    expect(optionImage?.alt).toBe('us-flag');
+  });
+
+  //prettier-ignore
+  it("should have \'en\' as english menu option href", async () => {
+    component.currentLang = 'es';
+    let matMenu = await loader.getHarness(MatMenuHarness);
+    await matMenu.open();
+    const option = el.query(By.css('.mat-mdc-menu-item'));
+    const optionHref = (option.nativeElement as HTMLLinkElement).href.slice(
+      (option.nativeElement as HTMLLinkElement).href.lastIndexOf('/') + 1
+    );
+    expect(optionHref).toBe('en');
   });
 });
