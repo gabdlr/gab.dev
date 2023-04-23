bc66f925000103e19987df16f217dc7b1d25e415
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-04-23 20:21:55 -0300

    test: control bar

--- src/app/components/controls-bar/controls-bar.component.spec.ts
+++ src/app/components/controls-bar/controls-bar.component.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ControlsBarComponent } from './controls-bar.component';
-
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
 describe('ControlsBarComponent', () => {
   let component: ControlsBarComponent;
   let fixture: ComponentFixture<ControlsBarComponent>;
-
+  let el: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ ControlsBarComponent ]
-    })
-    .compileComponents();
-
+      imports: [ControlsBarComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(ControlsBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,10 @@ describe('ControlsBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain language selector component', () => {
+    el = fixture.debugElement.query(By.directive(LanguageSelectorComponent));
+    expect(el).toBeTruthy();
   });
 });
